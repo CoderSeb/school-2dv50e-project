180747a06bbfa897d8ea1d6e06a8cb605722db6b
Sebastian Åkerblom <sebbeakerblom@gmail.com>
2023-02-22 15:33:41 +0100

    Add logic for implemented controller routes

--- ProductApi/src/main/java/lnu/exam/ProductApi/services/ProductService.java
+++ ProductApi/src/main/java/lnu/exam/ProductApi/services/ProductService.java
@@ -1,2 +1,30 @@
-package lnu.exam.ProductApi.services;public class ProductService {
+package lnu.exam.ProductApi.services;
+
+import java.util.List;
+import java.util.Objects;
+import lnu.exam.ProductApi.dtos.ProductDTO;
+import lnu.exam.ProductApi.exceptions.ResourceNotFoundException;
+import lnu.exam.ProductApi.models.Product;
+import lnu.exam.ProductApi.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+  @Autowired
+  private ProductRepository productRepo;
+
+  public List<Product> getAll() {
+    return productRepo.findAll();
+  }
+
+  public Product getById(Long id) throws ResourceNotFoundException {
+    return productRepo.findById(id)
+        .orElseThrow(() -> new ResourceNotFoundException("Product not found for this id: " + id));
+  }
+
+  public Product create(Product product) {
+
+    return productRepo.save(product);
+  }
 }
