70c60fe7f52bb7d46a71e33e1575cab3cc69192f
Sebastian Åkerblom <sebbeakerblom@gmail.com>
2023-02-22 15:34:28 +0100

    Add Product repository

--- ProductApi/src/main/java/lnu/exam/ProductApi/repositories/ProductRepository.java
+++ ProductApi/src/main/java/lnu/exam/ProductApi/repositories/ProductRepository.java
@@ -1,2 +1,8 @@
-package lnu.exam.ProductApi.repositories;public class ProductRepository {
+package lnu.exam.ProductApi.repositories;
+
+import lnu.exam.ProductApi.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
 }
