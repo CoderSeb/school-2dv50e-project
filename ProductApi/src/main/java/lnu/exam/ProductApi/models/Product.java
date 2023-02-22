7979f610e5f9ba7798187cb60688860246509884
Sebastian Åkerblom <sebbeakerblom@gmail.com>
2023-02-22 15:34:04 +0100

    Add Product model

--- ProductApi/src/main/java/lnu/exam/ProductApi/models/Product.java
+++ ProductApi/src/main/java/lnu/exam/ProductApi/models/Product.java
@@ -1,2 +1,40 @@
-package lnu.exam.ProductApi.models;public class Product {
+package lnu.exam.ProductApi.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products")
+@Setter
+@Getter
+public class Product {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+    private Long id;
+
+  @Column(name = "name", nullable = false)
+  private String name;
+
+  @Column(name = "description")
+  private String description;
+
+  @Column(name = "price", nullable = false)
+  private double price;
+
+  public Product() {
+
+  }
+
+  public Product(String name, String description, double price) {
+    this.name = name;
+    this.description = description;
+    this.price = price;
+  }
+
+  @Override
+  public String toString() {
+    return "Product(\nName: " + name + "\nDescription: " + description + "\nPrice: " + price + "\n)";
+  }
 }
