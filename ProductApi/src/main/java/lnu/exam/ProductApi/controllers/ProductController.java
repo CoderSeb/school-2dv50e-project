a4208000a94e52c1b86cf4143415a607673709c8
Sebastian Åkerblom <sebbeakerblom@gmail.com>
2023-02-22 15:33:14 +0100

    Add controller for products route, add read, readAll & create

--- ProductApi/src/main/java/lnu/exam/ProductApi/controllers/ProductController.java
+++ ProductApi/src/main/java/lnu/exam/ProductApi/controllers/ProductController.java
@@ -1,2 +1,33 @@
-package lnu.exam.ProductApi.controllers;public class ProductController {
+package lnu.exam.ProductApi.controllers;
+
+import java.util.List;
+import lnu.exam.ProductApi.exceptions.ResourceNotFoundException;
+import lnu.exam.ProductApi.models.Product;
+import lnu.exam.ProductApi.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+  @Autowired
+  private ProductService productService;
+
+  @GetMapping
+  public ResponseEntity<List<Product>> getAll() {
+    return ResponseEntity.ok(productService.getAll());
+  }
+
+  @GetMapping("/{id}")
+  public Product getById(@PathVariable(value = "id") Long productId) throws ResourceNotFoundException {
+    return productService.getById(productId);
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public Product create(@RequestBody Product product) {
+    return productService.create(product);
+  }
 }
