441a77dc2b1c999d6c748c629da513833d483f06
Sebastian Åkerblom <sebbeakerblom@gmail.com>
2023-03-08 13:08:08 +0100

    Add test for modify

--- ProductApi/src/test/java/lnu/exam/ProductApi/services/ProductServiceTest.java
+++ ProductApi/src/test/java/lnu/exam/ProductApi/services/ProductServiceTest.java
@@ -2,6 +2,8 @@ package lnu.exam.ProductApi.services;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -40,7 +42,7 @@ public class ProductServiceTest {
     }
 
     @Test
-    public void testGetAll() throws ResourceNotFoundException {
+    public void getAllShouldReturnListOfProducts() throws ResourceNotFoundException {
         Product p1 = new Product();
         p1.setName("Product 1");
         p1.setDescription("Description 1");
@@ -65,5 +67,29 @@ public class ProductServiceTest {
         assertEquals(expected.getContent().size(), actual.getContent().size());
         assertEquals(expected.getLink(LinkRelation.of("self")).get().getHref(), 
                      actual.getLink(LinkRelation.of("self")).get().getHref());
+    }
+
+    @Test
+    public void modifyShouldReturnUpdatedProduct() {
+        Product p1 = new Product();
+        p1.setId(42L);
+        p1.setName("Product 1");
+        p1.setDescription("Description 1");
+        p1.setPrice(10.0);
+
+        Product p2 = new Product();
+        p2.setId(42L);
+        p2.setName("Product 2");
+        p2.setDescription("Description 2");
+        p2.setPrice(20.0);
+
+        EntityModel<Product> expected = EntityModel.of(p2);
+        when(repository.findById(42L)).thenReturn(Optional.of(p1));
+        when(repository.save(p1)).thenReturn(p1);
+        when(assembler.toModel(p1)).thenReturn(expected);
+
+        EntityModel<Product> actual = service.modify(42L, p2);
+        assertEquals(
+            Objects.requireNonNull(expected.getContent()).getName(), Objects.requireNonNull(actual.getContent()).getName());
     }
 }
