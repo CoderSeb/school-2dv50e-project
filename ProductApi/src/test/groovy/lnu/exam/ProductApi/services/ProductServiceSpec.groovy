8665899b8e81d887e18bddadb902714e19264a6e
Sebastian Åkerblom <sebbeakerblom@gmail.com>
2023-03-09 15:09:06 +0100

    Add test for modify

--- ProductApi/src/test/groovy/lnu/exam/ProductApi/services/ProductServiceSpec.groovy
+++ ProductApi/src/test/groovy/lnu/exam/ProductApi/services/ProductServiceSpec.groovy
@@ -32,4 +32,30 @@ class ProductServiceSpec extends Specification {
         then: "result.getContent().name should be 'test product'"
         result.getContent().name == expected.getContent().name
     }
+
+    def "modify method should return updated product"() {
+        given:
+        Product p1 = new Product()
+        p1.setId(42L)
+        p1.setName("Product One")
+        p1.setDescription("Description One")
+        p1.setPrice(10.0)
+
+        Product p2 = new Product()
+        p2.setId(42L)
+        p2.setName("Product Two")
+        p2.setDescription("Description Two")
+        p2.setPrice(20.0)
+
+        EntityModel<Product> expected = assembler.toModel(p2)
+
+        and:
+        repository.findById(42L) >> Optional.of(p1)
+
+        when:
+        EntityModel<Product> result = productService.modify(42L, p2)
+
+        then:
+        result.getContent().name == expected.getContent().name
+    }
 }
