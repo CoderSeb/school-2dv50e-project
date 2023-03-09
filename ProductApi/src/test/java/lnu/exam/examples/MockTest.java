090b1c1ece240318e2e147b690b7c7fd6fd17ea8
Leopold Huber <leo.huber1@gmail.com>
2023-03-09 10:40:45 +0100

    updated mock test

---
+++ ProductApi/src/test/java/lnu/exam/examples/MockTest.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+
+public class MockTest {
+  class ClassToMock {
+    public String someMethod() {
+      return "original result";
+    }
+  }
+
+  @Test
+  public void shouldReturnMockedResult() {
+    // Create a mock object of the class to mock
+    ClassToMock mockObj = mock(ClassToMock.class);
+    when(mockObj.someMethod()).thenReturn("mocked result");
+
+    // Call the method being tested
+    String result = mockObj.someMethod();
+
+    // Verify that the method called the mock object as expected
+    verify(mockObj).someMethod();
+
+    // Assert the result
+    assertEquals("mocked result", result);
+  }
+}
